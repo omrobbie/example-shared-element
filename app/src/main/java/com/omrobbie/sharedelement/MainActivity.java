bef0802ba26fc3ceffa4b55729b08a68af98ed33
omrobbie <putu.roby@yahoo.com>
2017-08-28 11:21:26 +0800

    tambahkan detail activity

--- app/src/main/java/com/omrobbie/sharedelement/MainActivity.java
+++ app/src/main/java/com/omrobbie/sharedelement/MainActivity.java
@@ -2,6 +2,9 @@ package com.omrobbie.sharedelement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CardView cardView = (CardView) findViewById(R.id.cardView);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "CardView di click!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
